0976f7564098002b888b8ef9eed1f913995faa1e
ask0908 <askdfjaslk@naver.com>
2022-04-15 15:38:11 +0900

    work에 썼으면 true, 아니면 false가 들어가게 처리, 객체 결합

--- App.js
+++ App.js
@@ -4,28 +4,63 @@ import {
   Text,
   View,
   TouchableOpacity,
-  TouchableHighlight,
-  TouchableWithoutFeedback,
-  Pressable,
+  TextInput,
 } from "react-native";
+import React, { useState } from "react";
 import { theme } from "./Colors";
 
 export default function App() {
+  // 사용자가 Work 탭을 보고 있는지 확인할 state
+  const [working, setWorking] = useState(true);
+  // 사용자의 입력값을 감지할 때 사용할 state
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+  const addToDo = () => {
+    if (text === "") {
+      return;
+    }
+    // 3개의 객체 결합(Object.assign())
+    // 1번 인자 : 빈 객체, 2번 인자 : 이전 todo, 3번 인자 : 대상 객체
+    const newToDos = Object.assign({}, toDos, {
+      [Date.now()]: { text, work: working },
+    });
+    setToDos(newToDos);
+    setText("");
+  };
+  console.log(toDos);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.gray }}
+          >
+            Work
+          </Text>
         </TouchableOpacity>
-        <TouchableWithoutFeedback
-          underlayColor="red"
-          activeOpacity={0.5}
-          onPress={() => console.log("누름")}
-        >
-          <Text style={styles.btnText}>Travel</Text>
-        </TouchableWithoutFeedback>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.gray,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
       </View>
+      <TextInput
+        onSubmitEditing={addToDo}
+        onChangeText={onChangeText}
+        returnKeyType="done"
+        value={text}
+        placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+        style={styles.input}
+      />
     </View>
   );
 }
@@ -44,6 +79,13 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 44,
     fontWeight: "600",
-    color: "white",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
