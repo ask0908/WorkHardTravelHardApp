90685a0b1b484cdca39f0a0467743417654369fe
ask0908 <askdfjaslk@naver.com>
2022-04-15 14:05:38 +0900

    버튼 2개 생성 및 버튼 관련 컴포넌트 확인

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Pressable,
+} from "react-native";
+import { theme } from "./Colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <Text style={styles.btnText}>Work</Text>
-        <Text style={styles.btnText}>Travel</Text>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableWithoutFeedback
+          underlayColor="red"
+          activeOpacity={0.5}
+          onPress={() => console.log("누름")}
+        >
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableWithoutFeedback>
       </View>
     </View>
   );
@@ -16,13 +33,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000",
+    backgroundColor: theme.background,
+    paddingHorizontal: 20,
   },
   header: {
+    justifyContent: "space-between",
     flexDirection: "row",
     marginTop: 100,
   },
   btnText: {
+    fontSize: 44,
+    fontWeight: "600",
     color: "white",
   },
 });
---
+++ Colors.js
@@ -0,0 +1,4 @@
+export const theme = {
+  background: "black", // 라이트모드, 다크모드 구분
+  gray: "#3A3D40", // 버튼이 비활성화일 때 색깔
+};
